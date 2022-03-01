7dce92a3d49038f0de4c975b25b3db49692feb58
Guilherme Suusa da Cruz <gui.sdacruz6@users.noreply.github.com>
2022-03-01 16:06:39 -0300

    Finalização do ex014

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,1 +1,35 @@
-window.alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var msg2 = window.document.getElementById('msg2')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    msg.innerHTML= `Agora são ${hora} : ${minutos}`
+    if (hora >= 0 && hora < 12){
+        //BOM DIA
+        img.src = 'imagens/fotomanha.png'
+        document.body.style.backgroundColor = '#e2cd9f'
+        msg2.innerHTML = 'Bom dia!'
+        document.getElementById('msg2').style.color = '#e2cd9f'
+    }else if (hora >=12 && hora < 18){
+        //BOA TARDE
+        img.src = 'imagens/fototarde.png'
+        document.body.style.backgroundColor = '#b9846f'
+        msg2.innerHTML = 'Boa tarde!'
+        document.getElementById('msg2').style.color = '#b9846f'
+
+    }else{
+        //BOA NOITE
+        img.src = 'imagens/fotonoite.png'
+        document.body.style.backgroundColor = '#515154'
+        msg2.innerHTML = 'Boa noite!'
+        document.getElementById('msg2').style.color = '#515154'
+
+    }
+
+
+}
+
+setInterval(carregar, 500)
+
