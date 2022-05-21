21441cb0d3c9d00236ab0190664e9860ab27f352
Guilherme Suusa da Cruz <gui.sdacruz6@users.noreply.github.com>
2022-05-21 17:13:31 -0300

    Terminar o local

--- exercicios/LocaStorage/script.js
+++ exercicios/LocaStorage/script.js
@@ -5,3 +5,54 @@ if(localStorage.visitas){
     localStorage.visitas= 1
 }
 console.log(`Visitas: ${localStorage.visitas}`)
+
+var vcorf, vcortxt, vnome
+
+if(localStorage.nome){
+    vcorf = localStorage.corf
+    vcortxt = localStorage.cortxt
+    vnome = localStorage.nome
+    defineCor(1,vcorf)
+    defineCor(2,vcortxt)
+    document.getElementById('painel').innerHTML= `Olá, ${vnome}<br>`
+    document.getElementById('acesso').innerHTML+= ` ${Number(localStorage.visitas)} vezes`
+}
+
+function gravar(){
+    vnome = document.getElementById('fnome').value
+    let form = document.getElementById('fconf')
+    localStorage.corf=vcorf
+    localStorage.cortxt=vcortxt
+    localStorage.nome=vnome
+    document.getElementById('painel').innerHTML= `Olá, ${vnome}<br>`
+    document.getElementById('acesso').innerHTML= `Você acessou o site ${Number(localStorage.visitas)} vezes`
+    form.style.display='none'
+}
+
+document.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        let btn = document.getElementById('btn')
+        btn.click()
+    }
+})
+
+function defineCor(op,cor){
+    if(op==1){
+        document.body.style.backgroundColor=cor
+        vcorf=cor
+    }else{
+        document.body.style.color=cor
+        vcortxt=cor
+    }
+}
+
+function conf(p){
+    let form = document.getElementById('fconf')
+    if(p==1){
+       form.style.display='block' 
+    }else{
+        form.style.display='none'
+    }
+    
+}
+
